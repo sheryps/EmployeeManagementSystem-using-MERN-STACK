753c3fef1b29d0c20660901248182641112b780a
sheryps <sherrygeorge48@gmail.com>
2026-02-27 21:16:44 +0530

    updated

--- EMS_server/server.js
+++ EMS_server/server.js
@@ -18,6 +18,6 @@ app.use("/api/employees", require("./routes/employeeRoutes"));
 app.use("/api/dashboard", require("./routes/dashboardRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
