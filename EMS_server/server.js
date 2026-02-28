6d0a5a6d83cb3dbca6b942cbc1be4e247581bb01
sheryps <sherrygeorge48@gmail.com>
2026-02-28 11:58:19 +0530

    cors updated

--- EMS_server/server.js
+++ EMS_server/server.js
@@ -5,29 +5,15 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 
 const app = express();
-const allowedOrigins=["http://localhost:3000",   
-"https://employee-management-system-using-me.vercel.app"]
 connectDB();
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin (like Postman)
-      if (!origin) return callback(null, true);
-
-      if (
-        allowedOrigins.includes(origin) ||
-        origin.endsWith(".vercel.app")
-      ) {
-        return callback(null, true);
-      } else {
-        return callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: true,
     credentials: true,
   })
 );
-
+app.options("*",cors())
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
